707f95ee94d2d229d65f4c50f0201a129037bc86
pavel <pavel1367>
2025-07-02 22:06:41 +0300

    structs

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -5,33 +5,43 @@ struct User {
     sign_in_count: u64,
     active: bool,
 }
-fn main() {
-    let mut user1 = User {
-        email: String::from("pavel@gmail.com"),
-        username: String::from("pasha1367"),
-        sign_in_count: 1,
-        active: true,
-    };
 
-    let name = user1.username;
-    println!("Username: {}", name);
-    user1.username = String::from("pavel_changed");
-    println!("Username: {}", name);
-    println!("User1 {:?}", user1);
-
-    let user2 = build_user(String::from("pavel@mail.ru"), String::from("NIkita22"));
-    let user3 = User {
-        email: String::from("Johndole@gmail.com"),
-        username: String::from("james22"),
-        ..user2
-    };
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
 }
 
-fn build_user(email: String, username: String) -> User {
-    User {
-        email,
-        username,
-        sign_in_count: 1,
-        active: true,
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
     }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+    fn modify_width(&mut self, width: u32) {
+        self.width = width;
+    }
+}
+
+fn main() {
+    let rect = Rectangle {
+        width: 30,
+        height: 50,
+    };
+
+    let mut rect2 = Rectangle {
+        width: 10,
+        height: 20,
+    };
+    let rect3 = Rectangle {
+        width: 70,
+        height: 10,
+    };
+    println!("rect: {:#?}", rect);
+    println!("Can rect1 hold rect2? {}", rect.can_hold(&rect2));
+    rect2.modify_width(90);
+    println!("Can rect1 hold rect2? {}", rect.can_hold(&rect2));
+    println!("The area of rectangle is {} square pixels.", rect.area())
 }
