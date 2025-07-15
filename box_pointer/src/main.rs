933ed1806ac9d3673cbba23ed39623e210e8e0a5
pavel <pavel1367>
2025-07-15 22:45:36 +0300

    deref trait

--- box_pointer/src/main.rs
+++ box_pointer/src/main.rs
@@ -1,10 +1,40 @@
-enum List {
-    Cons(i32, Box<List>),
-    Nil,
+// enum List {
+//     Cons(i32, Box<List>),
+//     Nil,
+// }
+//
+// use List::{Cons, Nil};
+//
+// fn main() {
+//     let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+// }
+
+use std::ops::Deref;
+
+struct MyBox<T>(T);
+impl<T> MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
+}
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+
+    fn deref(&self) -> &T {
+        &self.0
+    }
 }
 
-use List::{Cons, Nil};
-
 fn main() {
-    let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+    let x = 5;
+    let y = MyBox::new(x);
+
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
+    let m = MyBox::new(String::from("Rust"));
+    hello(&m);
+}
+
+fn hello(name: &str) {
+    println!("hello {}!", name);
 }
